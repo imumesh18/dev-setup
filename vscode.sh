b0e9df1e98c8d808531d10b4d5e5e36e303e6e0e
Unknown <umesh4257@gmail.com>
2017-12-30 09:42:15 -0500

    Added comments and customised the banner

--- vscode.sh
+++ vscode.sh
@@ -4,7 +4,7 @@ set -e
 
 sudo -v
 
-# Changes the text color to Light-green.
+# Changes the text color to Light-Blue.
 echo -e "\\033[1;94m"
 
 cat << "EOF"
@@ -19,8 +19,33 @@ EOF
 # Changes the text color to default
 echo -e "\\033[0m"
 
+# Imports  Visual Studio Code gpg key
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+
+# Adds the Visual Studio Code apt PPA
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+# Update packages and Installs Visual Studio Code latest version 
 sudo apt-get update
-sudo apt-get install code
+sudo apt-get install -y code
+
+# Verify and exit installation
+echo -n "Verifying Visual Studio Code installation... "
+echo
+CODE_CHECK="$(code -v 3>&1)"
+if [[ "$CODE_CHECK" == *"x64"* ]]; then
+   echo -e "\\033[0;32mOK"
+   echo
+   echo "Visual Studio Code is successfully installed!"
+   echo
+   code -v
+   echo -e "\\033[0m"
+   exit 0
+else
+   echo -e "\\033[0;31mFAILED"
+   echo
+   echo "$0: Lol! Something went wrong, try to fix yourself else report an issues"
+   echo -e "\\033[0m"
+   exit 1
+fi
