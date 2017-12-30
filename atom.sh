941ae43fa67fcf90503fab54ed9e141e3843dc3e
Unknown <umesh4257@gmail.com>
2017-12-30 09:41:31 -0500

    Added comments and customised the banner

--- atom.sh
+++ atom.sh
@@ -4,8 +4,8 @@ set -e
 
 sudo -v
 
-# Changes the text color to Light-green.
-echo -e "\\033[1;92m"
+# Changes the text color to Grey.
+echo -e "\\033[1;38;5;59m"
 
 cat << "EOF"
  ___           _        _ _ _                  _   _                  
@@ -19,7 +19,32 @@ EOF
 # Changes the text color to default
 echo -e "\\033[0m"
 
+# Imports Atom text editor gpg key 
 curl -L https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+
+# Adds the Atom text editor apt PPA 
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+
+# Update packages and Installs Atom text editor latest version 
 sudo apt update
-sudo apt install atom
+sudo apt install -y atom
+
+# Verify and exit installation
+echo -n "Verifying Atom Text Editor installation... "
+echo
+ATOM_CHECK="$(atom -v 1>&1)"
+if [[ "$ATOM_CHECK" == *"Atom"* ]]; then
+   echo -e "\\033[0;32mOK"
+   echo
+   echo "Atom Text Editor is successfully installed!"
+   echo
+   atom -v
+   echo -e "\\033[1mHACK THE WORLD!\\033[0m"
+   exit 0
+else
+   echo -e "\\033[0;31mFAILED"
+   echo
+   echo "$0: Lol! Something went wrong, try to fix yourself else report an issues"
+   echo -e "\\033[0m"
+   exit 1
+fi
