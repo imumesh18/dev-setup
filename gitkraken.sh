6a7749cd94d6c540b3aa7f0a27cf2a7af90e084c
Unknown <umesh4257@gmail.com>
2017-12-30 09:41:55 -0500

    Added comments and customised the banner

--- gitkraken.sh
+++ gitkraken.sh
@@ -2,12 +2,14 @@
 
 set -e
 
+sudo -v
+
 # Install dependency for gitkraken
 echo -e "\\033[1;92m**Installing dependencies for Gitkraken**\\033[1;92m\\033[0m"
 sudo apt install libgnome-keyring-dev libcurl3
 
-# Changes the text color to Light-green.
-echo -e "\\033[1;92m"
+# Changes the text color to Light-Green.
+echo -e "\\033[1;38;5;23m"
 
 cat << "EOF"
  ___           _        _ _ _                ____ _ _   _              _              
@@ -21,6 +23,31 @@ EOF
 # Changes the text color to default
 echo -e "\\033[1;92m\\033[0m"
 
-wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+# Downloads gitkraken .deb package for ubuntu
+wget https://release.gitkraken.com/linux/gitkraken-amd64.deb 
+
+# Installs gitkraken
 dpkg -i gitkraken-amd64.deb
+
+# Removes gitkraken .deb package from machine
 rm -f gitkraken-amd64.deb
+
+# Verify and exit installation
+echo -n "Verifying Gitkraken installation... "
+echo
+GITKRAKEN_CHECK="$(gitkraken -v 1>&1)"
+if [[ "$GITKRAKEN_CHECK" == *"XNode started time"* ]]; then
+   echo -e "\\033[0;32mOK"
+   echo
+   echo "Gitkraken is successfully installed!"
+   echo
+   atom -v
+   echo -e "\\033[0m"
+   exit 0
+else
+   echo -e "\\033[0;31mFAILED"
+   echo
+   echo "$0: Lol! Something went wrong, try to fix yourself else report an issues"
+   echo -e "\\033[0m"
+   exit 1
+fi
